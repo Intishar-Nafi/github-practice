8433e9eb1f0917f92ca1dfb5fc91eeb00ff7f5b4
Intishar Rahman Nafi <initshar.nafi@tigerit.com>
2025-04-15 12:37:24 +0600

    new Function addedd in temp.py

--- temp.py
+++ temp.py
@@ -3,5 +3,10 @@ import os
 import json
 def func(temp, bump, prep, ash):
         return 0
+<<<<<<< Updated upstream
 def newFunc(car, bike, ship):
         return 1
+=======
+def newFunc(cycle, ship):
+        return 0
+>>>>>>> Stashed changes
